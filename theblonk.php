6a1614ee6cd29944304e538d1fef77c1aab6221a
theBlonk <theBlonk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2010-07-10 00:16:40 +0000

    php file error fixed
    git-svn-id: https://plugins.svn.wordpress.org/theblonk/trunk@262192 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- theblonk.php
+++ theblonk.php
@@ -1,2 +1,2 @@
 <br />
-<b>Fatal error</b>:  Call to undefined function add_action() in <b>/home/socialap/public_html/test/theblonk.php</b> on line <b>18</b><br />
+<b>Fatal error</b>:  Call to undefined function add_action() in <b>/home/socialap/public_html/test/theblonk.php</b> on line <b>20</b><br />
